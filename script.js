ac1e73d60aee300c83673708daba5935d6a6d6c7
jakir9324 <jakirhusen9324@gmail.com>
2025-01-29 20:55:18 +0600

    Js var, let, const and simple problem solving

---
+++ script.js
@@ -0,0 +1,34 @@
+let x = 40;
+var y = 30;
+
+let amount;
+var number;
+
+amount = 60;
+number = 4;
+
+totalAmount = amount*number;
+console.log(totalAmount);
+
+amount = 90;
+number = 5;
+
+finalAmount = amount*number;
+
+console.log(finalAmount);
+
+
+let rahimTotalmany = 500;
+let egg = 5;
+let kitkat = 2;
+let eachEgg = 15;
+let eachKitkat = 40;
+
+let eggPrice = egg*eachEgg;
+let kitkatPrice = kitkat*eachKitkat;
+
+let totalPrice = eggPrice+kitkatPrice;
+
+let remainingMony = rahimTotalmany-totalPrice;
+
+console.log(remainingMony);
